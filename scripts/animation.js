60d65c717e69c7c88bbfaa86ef62c513274d596d
Gurikin <igor.banchikov@gmail.com>
2018-07-17 23:22:42 +0300

    Add the russian & english content & contacts

--- scripts/animation.js
+++ scripts/animation.js
@@ -2,8 +2,9 @@ $(function () {
     $('.service-card').on('click', function () {
         $('#modalWindow').fadeToggle(1000);
         var id = $(this).attr('id');
+        console.log(id);
         var lang = (location.href.indexOf('ru')+1) ? 'ru' : 'en'
-        var source = '/cards/' + id + '-' + lang + '.html';
+        var source = './cards/' + id + '-' + lang + '.html';
         $.get(source, function (data) {
             $("#modalWindow-content").html(data);
         });
